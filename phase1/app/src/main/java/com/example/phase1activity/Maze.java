8dfebe71dbd65ca9e032435c5ebff14bf5073182
Ramzi Dajani <ramzi.dajani@mail.utoronto.ca>
2019-10-21 13:53:41 -0400

    Established MazeItem inheritance and created x and y attributes for MazeItem

--- phase1/app/src/main/java/com/example/phase1activity/Maze.java
+++ phase1/app/src/main/java/com/example/phase1activity/Maze.java
@@ -1,4 +1,14 @@
 package com.example.phase1activity;
 
 public class Maze {
+
+    public ArrayList<Wall> walls;
+    public ArrayList<MazeBlock> blocks;
+
+    public MazeBlock winBlock;
+
+    public Maze(){
+        walls = new ArrayList<>();
+        blocks = new ArrayList<>();
+    }
 }

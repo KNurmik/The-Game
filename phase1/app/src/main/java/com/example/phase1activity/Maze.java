9d1f80ad3f97da5322cd254c73249bc53ff0d057
Henry Leung <henr.leung@mail.utoronto.ca>
2019-10-25 13:51:20 -0400

    Commit Maze.java changes

--- phase1/app/src/main/java/com/example/phase1activity/Maze.java
+++ phase1/app/src/main/java/com/example/phase1activity/Maze.java
@@ -1,14 +1,4 @@
 package com.example.phase1activity;
 
 public class Maze {
-
-    public ArrayList<Wall> walls;
-    public ArrayList<MazeBlock> blocks;
-
-    public MazeBlock winBlock;
-
-    public Maze(){
-        walls = new ArrayList<>();
-        blocks = new ArrayList<>();
-    }
 }

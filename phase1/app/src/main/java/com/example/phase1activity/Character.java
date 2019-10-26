8a7c0501f4d3d77cf3584a45ef2495ecc5e2c738
Henry Leung <henr.leung@mail.utoronto.ca>
2019-10-26 18:32:51 -0400

    Created the appearance of the walls and character and added a draw method.

--- phase1/app/src/main/java/com/example/phase1activity/Character.java
+++ phase1/app/src/main/java/com/example/phase1activity/Character.java
@@ -1,11 +1,25 @@
 package com.example.phase1activity;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.Paint;
+
 public class Character {
 
     public MazeBlock currentBlock;
+    public String appearance;
+
+    private Paint paintText =new Paint();
 
     public Character(MazeBlock block) {
         currentBlock = block;
+
+        paintText.setTextSize(36);
+        paintText.setColor(Color.BLUE);
+        paintText.setTypeface(Typeface.DEFAULT_BOLD);
+
+        appearance = "o";
     }
 
     enum Direction {
@@ -35,5 +49,8 @@ public class Character {
                 }
                 break;
         }
+    }
+    public void draw(Canvas canvas) {
+        canvas.drawText(appearance, currentBlock.getX(), currentBlock.getY(), paintText);
     }
 }
--- phase1/app/src/main/java/com/example/phase1activity/Wall.java
+++ phase1/app/src/main/java/com/example/phase1activity/Wall.java
@@ -1,17 +1,38 @@
 package com.example.phase1activity;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.graphics.Paint;
+
 public class Wall extends MazeItem {
 
     public boolean horizontal;
+    public String appearance;
+
+    private Paint paintText =new Paint();
 
     public Wall(int a, int b, boolean horz) {
         super(a, b);
         horizontal = horz;
+
+        if (horz){
+            appearance = "_";
+        }
+        else {
+            appearance = "|";
+        }
+
+        paintText.setTextSize(36);
+        paintText.setColor(Color.WHITE);
+        paintText.setTypeface(Typeface.DEFAULT_BOLD);
     }
 
     public boolean isHorz() {
         return horizontal;
     }
-
+    public void draw(Canvas canvas) {
+        canvas.drawText(appearance, getX(), getY(), paintText);
+    }
 
 }

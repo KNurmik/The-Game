9d0be33a53bf4da76334b689a7513e4ff27cd584
dajanir1 <ramzi.dajani@mail.utoronto.ca>
2019-10-26 16:32:57 -0400

    MazeBlocks now have up, down, left, and right attributes

--- phase1/app/src/main/java/com/example/phase1activity/MazeBlock.java
+++ phase1/app/src/main/java/com/example/phase1activity/MazeBlock.java
@@ -2,7 +2,48 @@ package com.example.phase1activity;
 
 public class MazeBlock extends MazeItem {
 
+    private MazeBlock up;
+    private MazeBlock down;
+    private MazeBlock right;
+    private MazeBlock left;
+
     public MazeBlock(double a, double b){
         super(a, b);
+        up = null;
+        down = null;
+        right = null;
+        left = null;
+    }
+
+    public MazeBlock getUp() {
+        return up;
+    }
+
+    public void setUp(MazeBlock up) {
+        this.up = up;
+    }
+
+    public MazeBlock getDown() {
+        return down;
+    }
+
+    public void setDown(MazeBlock down) {
+        this.down = down;
+    }
+
+    public MazeBlock getRight() {
+        return right;
+    }
+
+    public void setRight(MazeBlock right) {
+        this.right = right;
+    }
+
+    public MazeBlock getLeft() {
+        return left;
+    }
+
+    public void setLeft(MazeBlock left) {
+        this.left = left;
     }
 }

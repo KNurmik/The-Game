146c3bd4ca08ab4fef2a0f8d603c8cf7ec902e58
Ramzi Dajani <ramzi.dajani@mail.utoronto.ca>
2019-10-26 18:21:48 -0400

    Added Getters/Setters for MazeItem.x and MazeItem.y

--- phase1/app/src/main/java/com/example/phase1activity/MazeItem.java
+++ phase1/app/src/main/java/com/example/phase1activity/MazeItem.java
@@ -12,8 +12,46 @@ public class MazeItem {
      */
     private int y;
 
+    /**
+     * The MazeItem constructor
+     * @param a The initial x value of the MazeItem
+     * @param b The initial y value of the MazeItem
+     */
     public MazeItem(int a, int b) {
         x = a;
         y = b;
     }
+
+    /**
+     * Getter for MazeItem.x
+     * @return x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Setter for MazeItem.x
+     * @param x The new x value of this MazeItem
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * Getter for MazeItem.y
+     * @return y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Setter for MazeItem.y
+     * @param y The new y value of this MazeItem
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
 }

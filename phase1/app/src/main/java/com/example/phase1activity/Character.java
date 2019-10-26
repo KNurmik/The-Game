29012088da739e1f4c4ea81371da8bac7a79b9be
Ramzi Dajani <ramzi.dajani@mail.utoronto.ca>
2019-10-26 17:40:40 -0400

    Bug fix

--- phase1/app/src/main/java/com/example/phase1activity/Character.java
+++ phase1/app/src/main/java/com/example/phase1activity/Character.java
@@ -1,11 +1,11 @@
 package com.example.phase1activity;
 
-public class Character extends MazeItem{
+public class Character {
 
     public MazeBlock currentBlock;
 
-    public Character(double a, double b) {
-        super(a, b);
+    public Character(MazeBlock block) {
+        currentBlock = block;
     }
 
     enum Direction{
@@ -15,7 +15,25 @@ public class Character extends MazeItem{
     public void move(Direction direction){
         switch(direction){
             case UP:
-
+                if (currentBlock.getUp() instanceof MazeBlock){
+                    currentBlock = (MazeBlock) currentBlock.getUp();
+                }
+                break;
+            case DOWN:
+                if (currentBlock.getDown() instanceof MazeBlock){
+                    currentBlock = (MazeBlock) currentBlock.getDown();
+                }
+                break;
+            case LEFT:
+                if (currentBlock.getLeft() instanceof MazeBlock){
+                    currentBlock = (MazeBlock) currentBlock.getLeft();
+                }
+                break;
+            case RIGHT:
+                if (currentBlock.getRight() instanceof MazeBlock){
+                    currentBlock = (MazeBlock) currentBlock.getRight();
+                }
+                break;
         }
     }
 }

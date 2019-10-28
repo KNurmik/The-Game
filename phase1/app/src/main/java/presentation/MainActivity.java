3c986272712fa933d3de08bfc393c1022063632b
Karl Hendrik Nurmeots <karl.nurmeots@mail.utoronto.ca>
2019-10-28 19:33:20 -0400

    Created "presentation" package. Moved MainActivity to this package.

--- phase1/app/src/main/java/presentation/MainActivity.java
+++ phase1/app/src/main/java/presentation/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.phase1activity;
+package presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.phase1activity.R;
 
 public class MainActivity extends AppCompatActivity {
 

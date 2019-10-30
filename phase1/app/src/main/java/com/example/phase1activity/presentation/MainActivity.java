9d808a07f29b599727ad056635f63a8a53a3e45d
Karl Hendrik Nurmeots <karl.nurmeots@mail.utoronto.ca>
2019-10-30 15:02:12 -0400

    Created LogInActivity, RegisterActivity.

---
+++ phase1/app/src/main/java/com/example/phase1activity/presentation/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.phase1activity.presentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.phase1activity.R;
+import com.example.phase1activity.presentation.LogIn.LogInActivity;
+import com.example.phase1activity.presentation.Register.RegisterActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button logInBtn = findViewById(R.id.logInButton);
+
+        logInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LogInActivity.class));
+            }
+        });
+
+        Button regBtn = findViewById(R.id.createAccountButton);
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
---
+++ phase1/app/src/main/java/com/example/phase1activity/presentation/Register/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.phase1activity.presentation.Register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.phase1activity.R;
+import com.example.phase1activity.presentation.MainActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+    }
+}

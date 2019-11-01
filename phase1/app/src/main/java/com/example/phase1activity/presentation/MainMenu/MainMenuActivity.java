138b05a926d1bb0b4408a898e069edabdf23870d
Olivia Li <oliviaa.li@mail.utoronto.ca>
2019-11-01 02:34:26 -0400

    fixed music issue. Works across activities now

--- phase1/app/src/main/java/com/example/phase1activity/presentation/MainMenu/MainMenuActivity.java
+++ phase1/app/src/main/java/com/example/phase1activity/presentation/MainMenu/MainMenuActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.phase1activity.Profile.AppManager;
 import com.example.phase1activity.R;
 
 public class MainMenuActivity extends AppCompatActivity {
+    AppManager app;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        app = (AppManager) getApplication();
+
     }
 }

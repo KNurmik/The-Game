cc1087b785e7d54a543646e55ce6836613a042a6
Karl Hendrik Nurmeots <karl.nurmeots@mail.utoronto.ca>
2019-11-29 14:43:38 -0500

    Added Strategy and DI design patterns to GlobalStats.

---
+++ phase2/app/src/main/java/com/example/phase1activity/UI/MenuScreens/LeaderBoardByReactionTime.java
@@ -0,0 +1,49 @@
+package com.example.phase1activity.UI.MenuScreens;
+
+import com.example.phase1activity.Core.Transmission.Saving.AndroidSaver;
+import com.example.phase1activity.Core.Transmission.Saving.ISaver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+/** Strategy for sorting players based on their fastest reaction time. */
+public class LeaderBoardByReactionTime implements LeaderBoardSorting {
+
+  /** Tell Dagger how to create this object. */
+  @Inject
+  public LeaderBoardByReactionTime() {}
+
+  /**
+   * Sort players based on their fastest reaction time, return a list of players and their
+   * statistic.
+   *
+   * @param iSaver the saver to read the data from.
+   * @return a list of players along with their statistics.
+   */
+  public List<List<Object>> sortPlayers(ISaver iSaver) {
+    List<List<Object>> usersWithFastestReactions = new ArrayList<>();
+    for (String username : iSaver.getHighScores().keySet()) {
+      double userFastestReaction =
+          iSaver.getHighScores().get(username).get(AndroidSaver.AttributeType.FASTEST_RXN_TIME);
+      List<Object> listEntry = new ArrayList<>();
+      listEntry.add(0, username);
+      listEntry.add(1, userFastestReaction);
+
+      boolean userAddedToList = false;
+
+      for (int i = 0; i < usersWithFastestReactions.size(); i++) {
+        if (userFastestReaction < (double) usersWithFastestReactions.get(i).get(1)) {
+          usersWithFastestReactions.add(i, listEntry);
+          userAddedToList = true;
+          break;
+        }
+      }
+      if (!userAddedToList) {
+        usersWithFastestReactions.add(listEntry);
+      }
+    }
+    return usersWithFastestReactions;
+  }
+}
